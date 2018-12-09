ffc44d0510ee205ccbf2da86c236e30b104f61d7
Zhigang Gao <gzg1984@aliyun.com>
2018-12-09 17:48:01 +0800

    First swift test

--- Lesson2/ViewController.swift
+++ Lesson2/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+var Teststr = "Hello, playground"
 
 class ViewController: UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = Teststr
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
 
+
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+}
